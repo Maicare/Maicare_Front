702c857aa9b83b4949db38decbc3279f11e09f16
bourichi taha <bourichi.taha@gmail.com>
2025-01-13 00:26:14 +0100

    index api routes

---
+++ common/api/routes.ts
@@ -0,0 +1,11 @@
+import Auth from './routes/auth';
+import Employee from './routes/employee';
+import Role from './routes/role';
+
+const ApiRoutes = {
+  Auth,
+  Employee,
+  Role
+};
+
+export default ApiRoutes;
