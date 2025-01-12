6b6c78d0f94e9e1fc1538456f2dce9edce92567a
bourichi taha <bourichi.taha@gmail.com>
2025-01-13 00:35:43 +0100

    main site layout and dashboard page using withAuth and withPermission

---
+++ app/(pages)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { PermissionsObjects } from '@/common/data/permission.data';
+import withAuth, { AUTH_MODE } from '@/common/hocs/with-auth';
+import withPermissions from '@/common/hocs/with-permissions';
+import Routes from '@/common/routes';
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>dashboard page</div>
+  )
+}
+
+export default withAuth(withPermissions(DashboardPage,{redirectUrl:Routes.Common.NotFound,requiredPermissions:PermissionsObjects.ViewDashboard}), { mode: AUTH_MODE.LOGGED_IN, redirectUrl: Routes.Auth.Login });
