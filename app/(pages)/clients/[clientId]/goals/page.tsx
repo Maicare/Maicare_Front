d6ba05239fafb7c742bb3c76f4b3741781915f55
bourichi taha <bourichi.taha@gmail.com>
2025-02-24 19:20:36 +0000

    fixing not visibale styling

--- app/(pages)/clients/[clientId]/goals/page.tsx
+++ app/(pages)/clients/[clientId]/goals/page.tsx
@@ -6,7 +6,7 @@ import Panel from '@/components/common/Panel/Panel';
 import { useAssessment } from '@/hooks/assessment/use-assessment';
 import { AssessmentResponse } from '@/types/assessment.types';
 import { LEVEL_OPTIONS } from '@/types/maturity-matrix.types';
-import { cn, getTailwindClasses } from '@/utils/cn';
+import { cn } from '@/utils/cn';
 import { fullDateFormat } from '@/utils/timeFormatting';
 import { ColumnDef } from '@tanstack/table-core';
 import { CheckCircleIcon, XCircleIcon } from 'lucide-react';
@@ -18,7 +18,22 @@ const GoalPage = () => {
   const [page, setPage] = useState<number>(1);
   const { assessments, isLoading, error } = useAssessment({ autoFetch: true, clientId: parseInt(clientId as string), page, page_size: 10 });
   const router = useRouter();
-  
+  const getTailwindClasses = (level: number) => {
+    switch (level) {
+      case 1:
+        return "bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300";
+      case 2:
+        return "bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-yellow-900 dark:text-yellow-300";
+      case 3:
+        return "bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300";
+      case 4:
+        return "bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300";
+      case 5:
+        return "bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-purple-900 dark:text-purple-300";
+      default:
+        return "bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300";
+    }
+  };
   const columnDef = useMemo<ColumnDef<AssessmentResponse>[]>(() => {
     return [
       {
@@ -38,7 +53,7 @@ const GoalPage = () => {
           const level = info.getValue() as number;
           const classes = getTailwindClasses(level);
           return (
-          <span className={cn(classes)}>{LEVEL_OPTIONS.find(it => it.value === info.getValue().toString())?.label || "Niet Beschikbaar"}</span>
+          <span className={cn("p-2",classes)}>{LEVEL_OPTIONS.find(it => it.value === info.getValue().toString())?.label || "Niet Beschikbaar"}</span>
         )
         },
       },
